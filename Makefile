61d381ff9fe163ff95e3da14a8f9d54eb561f09e
Vladimir Zidar <mr_w@mindnever.org>
2016-12-08 18:27:47 +0100

    Iinitial commit

---
+++ Makefile
@@ -0,0 +1,48 @@
+PORT = /dev/tty.SLAB_USBtoUART
+DEVICE  = atmega8
+F_CPU   = 8000000	# in Hz
+
+AVRDUDE = avrdude -c stk500v2 -P $(PORT) -p $(DEVICE) -v -b9600
+COMPILE = avr-gcc -Os -Wall -I. -mmcu=$(DEVICE) -DF_CPU=$(F_CPU) -D__AVR_LIBC_DEPRECATED_ENABLE__ -DDEBUG_LEVEL=1 -std=c99
+OBJECTS = main.o
+
+
+# symbolic targets:
+all:	main.hex
+
+.c.o:
+	$(COMPILE) -c $< -o $@
+
+.S.o:
+	$(COMPILE) -x assembler-with-cpp -c $< -o $@
+# "-x assembler-with-cpp" should not be necessary since this is the default
+# file type for the .S (with capital S) extension. However, upper case
+# characters are not always preserved on Windows. To ensure WinAVR
+# compatibility define the file type manually.
+
+.c.s:
+	$(COMPILE) -S $< -o $@
+
+clean:
+	rm -f $(OBJECTS) main.hex
+	
+# file targets:
+main.bin:	$(OBJECTS)
+	$(COMPILE) -o main.bin $(OBJECTS)
+
+main.hex:	main.bin
+	rm -f main.hex main.eep.hex
+	avr-objcopy -j .text -j .data -O ihex main.bin main.hex
+	./checksize main.bin 8912 960
+
+disasm:	main.bin
+	avr-objdump -d main.bin
+
+cpp:
+	$(COMPILE) -E main.c
+
+flash: main.hex
+	$(AVRDUDE) -U main.hex
+
+#fuse:
+#	$(AVRDUDE) -U lfuse:w:0x9f:m -U hfuse:w:0xc9:m
---
+++ main.c
@@ -0,0 +1,169 @@
+#define F_CPU 8000000UL   //CPU-freq
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define CpFET_PIN           5
+#define CpFET_PORT          D
+
+#define BpFET_PIN           4
+#define BpFET_PORT          D
+
+#define ApFET_PIN           3
+#define ApFET_PORT          D
+
+#define CnFET_PIN              2
+#define CnFET_PORT             B
+
+#define BnFET_PIN              1
+#define BnFET_PORT             B
+
+#define AnFET_PIN              0
+#define AnFET_PORT             B
+
+
+#define Rcp_In_PIN          2
+#define Rcp_In_PORT         D
+
+
+//#define Mux_C           PC5
+//#define Mux_B           PC4
+//#define Temp_Ip         PC1
+//#define Volt_Ip         PC0
+
+
+//#define DebugPin           PB4
+
+#define _CONCAT(x,y) x##y
+
+#define _DDR(x) DDR##x
+#define _PORT(x) PORT##x
+#define _PIN(x) PIN##x
+
+#define DDR(x) _DDR(x)
+#define PORT(x) _PORT(x)
+#define PIN(x) _PIN(x)
+
+#define INIT_OUT(x) DDR(_CONCAT(x, _PORT)) |= (1 << (x ## _PIN))
+#define INIT_IN(x) DDR(_CONCAT(x, _PORT)) &= ~(1 << (x ## _PIN))
+
+#define ON(x) PORT(_CONCAT(x, _PORT)) |= (1 << (x ## _PIN))
+#define OFF(x) PORT(_CONCAT(x, _PORT)) &= ~(1 << (x ## _PIN))
+
+#define IN(x) (PIN(_CONCAT(x, _PORT)) & (1 << (x ## _PIN)))
+
+int main(void)
+{
+    OFF(CpFET);
+    OFF(BpFET);
+    OFF(ApFET);
+    
+    OFF(CnFET);
+    OFF(BnFET);
+    OFF(AnFET);
+    
+    INIT_OUT(CpFET);
+    INIT_OUT(BpFET);
+    INIT_OUT(ApFET);
+    
+    INIT_OUT(CnFET);
+    INIT_OUT(BnFET);
+    INIT_OUT(AnFET);
+    
+    INIT_IN(Rcp_In);
+    
+    
+    uint16_t timeUPStart = 0;
+    uint16_t timeUPEnd = 0;
+    uint16_t Pulse = 0;
+    
+    // 1MHz timer clock = 1uS precission
+    // TIMER1: need to measure servo pulses in microseconds (4000uS)
+    
+    TCCR1A = 0;
+    TCCR1B = _BV(CS11); // prescaler: clock/8
+    TIMSK = _BV(TOIE1); // overflow interrupt enable
+    
+    
+    sei();
+    
+    uint8_t trigged = 0;
+    
+    uint8_t forward = 0;
+    
+    uint16_t PWMMargin = 15;
+    uint16_t PWMMiddle = 1500;
+    uint16_t PWMMin = PWMMiddle - 512;
+    uint16_t PWMMax = PWMMiddle + 512;
+
+    while(1) {
+        
+        if (!trigged && IN(Rcp_In)) {
+            timeUPStart = TCNT1;        /* capture the time when the signal when high*/
+            trigged = 1;
+        }
+
+        if (trigged && !IN(Rcp_In)) {
+            timeUPEnd = TCNT1;  /* capture when the pulse goes low */
+            uint16_t p = (timeUPEnd - timeUPStart);
+            trigged = 0;
+            
+            if((p < PWMMin) || (p > PWMMax)) {
+
+                Pulse = 0;
+
+            } else if(p < (PWMMiddle - PWMMargin)) {
+
+                forward = 0;
+                Pulse = PWMMiddle - p;
+
+            } else if(p > (PWMMiddle + PWMMargin)) {
+
+                forward = 1;
+                Pulse = p - PWMMiddle;
+
+            } else {
+
+                Pulse = 0;
+            }
+            
+        }
+        
+        /* pulse 0-512 */
+        
+        uint8_t on = (TCNT1 & 0x1F) < Pulse;
+        
+        if(!on) {
+
+            OFF(ApFET);
+            OFF(AnFET);
+            OFF(CpFET);
+            OFF(CnFET);
+
+        } else if(forward) {
+            // Forward //
+
+            OFF(AnFET);
+            OFF(CpFET);
+            
+            ON(CnFET);
+            ON(ApFET);
+            
+        } else {
+            
+            // Reverse //
+            
+            OFF(ApFET);
+            OFF(CnFET);
+            
+            ON(CpFET);
+            ON(AnFET);
+            
+        }
+    }
+}
+
+ISR (TIMER1_OVF_vect)
+{
+    
+}
